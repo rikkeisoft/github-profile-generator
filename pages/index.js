19d3d472cac10fafbae27f9ffb8acc317bf7af3c
LeDucLoi <loild@rikkeisoft.com>
2021-07-23 17:56:47 +0700

    make coffee button

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,15 @@
+import Coffee from '@components/widgets/Coffee'
 import Footer from '../src/components/widgets/Footer'
 import Header from '../src/components/widgets/Headers'
 import Main from '../src/components/widgets/Main'
 
 export default function Home() {
   return (
-    <div>
+    <div className="z-0">
       <Header />
       <Main />
       <Footer />
+      <Coffee />
     </div>
   )
 }
